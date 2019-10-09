b1393f5002164d013598f291855cb40e79cc5cec
Simona Cotin <simona.cotin@gmail.com>
2019-10-09 18:05:44 +0100

    graphql endpoint

--- graphlapi/index.js
+++ graphlapi/index.js
@@ -1,16 +1,31 @@
+const {graphql, buildSchema} = require("graphql")
+const axios = require('axios')
+const schema = buildSchema(`
+    type Team {
+        id: ID
+        points: Int
+        name: String
+    }
+
+    type Query {
+        teams:[Team]
+    }
+`)
+
+const resolvers = {
+    teams: () => {
+        return axios.get("https://graphqlvoting.azurewebsites.net/api/score").then(res => res.data)
+    } 
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    
+    const teams = await graphql(schema, req.body.query, resolvers)
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: teams
+    };
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello Concatenate, you are awesome!!!" + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    
 };
